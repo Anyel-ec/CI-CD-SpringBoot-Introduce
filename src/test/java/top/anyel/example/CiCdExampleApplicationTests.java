e47b1425e0e58dbba624eab1b20a98e8f238b9f4
anyel-ec <paulangel826@gmail.com>
2025-07-01 12:01:07 -0500

    feat: upload feature with controller

---
+++ src/test/java/top/anyel/example/CiCdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package top.anyel.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
